451bfe5bba5af7e87dbb8539078e53a332581a71
jihyun-j <jihyun9142@gmail.com>
2023-10-25 20:52:48 +0900

    영화제목/배우 검색 1차

--- popularMovie.js
+++ popularMovie.js
@@ -1,133 +1,163 @@
 const url = `https://api.themoviedb.org/3/movie`;
-const searchUrl = `https://api.themoviedb.org/3/search/movie`;
-const imgUrl = `https://image.tmdb.org/t/p/original`
+const searchMultiUrl = `https://api.themoviedb.org/3/search/multi`;
+const imgUrl = `https://image.tmdb.org/t/p/original`;
+const emptyImg = `https://s3-us-west-1.amazonaws.com/files.delesign/assets/Not-Found-1.svg`; // image가 null 일때
 
 const options = {
-	method: 'GET',
-	headers: {
-		accept: 'application/json',
-		Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZmMyZGZhZDQ3YThlZDRmMWUwYWQxYjc1MGVhMzBhMSIsInN1YiI6IjY1MzA5NGQ3YWVkZTU5MDE0YzM4MDBjZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.F6qLqVKcX12Lxl8WUe5P3sDfhlIdJ44DaMgj0Dvuq1M'
-  	}
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZmMyZGZhZDQ3YThlZDRmMWUwYWQxYjc1MGVhMzBhMSIsInN1YiI6IjY1MzA5NGQ3YWVkZTU5MDE0YzM4MDBjZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.F6qLqVKcX12Lxl8WUe5P3sDfhlIdJ44DaMgj0Dvuq1M",
+  },
 };
 
-
-window.addEventListener('load', () => {
-	document.querySelector(`#search`).focus();
-})
+window.addEventListener("load", () => {
+  document.querySelector(`#search`).focus();
+});
 let movieList = [];
 
 const scoreModal = () => {
-	document.querySelector('#modal').style.display = 'block';
-}
+  document.querySelector("#modal").style.display = "block";
+};
 const closeModal = () => {
-	document.querySelector('#modal').style.display = 'none';
-	document.querySelector('#score').value = ``;
-}
+  document.querySelector("#modal").style.display = "none";
+  document.querySelector("#score").value = ``;
+};
 
 const makeMovieCard = (movieId, postImg, movieTitle, voteAverage, overView) => {
-	const movieCard = document.createElement('div');
+  const movieCard = document.createElement("div");
 
-	movieCard.className = 'movieCard';
-	movieCard.addEventListener('click', () => alert(`Movie id : ${movieId}`))
-	movieCard.innerHTML = `<div class="moviePoster">
-								<img src=${postImg} alt="">
+  // 영화 title이 undefined로 나올때 name을 보여준다
+  // 영화 포스터가 없을 때 다른 이미지를 보여준다
+  movieCard.className = "movieCard";
+  movieCard.addEventListener("click", () => alert(`Movie id : ${movieId}`));
+  movieCard.innerHTML = `<div class="moviePoster">
+								<img src=${postImg === null ? emptyImg : postImg} alt="">
 							</div>
-							<div class="movieTitle">${movieTitle}</div>
+							<div class="movieTitle">${movieTitle}</div> 
 							<div class="voteAverage" >🍅 : <span id="voteAverage">${voteAverage}</span></div>
-							<div class="overView">${overView}</div>`
+							<div class="overView">${overView}</div>`;
 
-	return document.querySelector("#movieList").appendChild(movieCard)
-}
+  return document.querySelector("#movieList").appendChild(movieCard);
+};
 
-  //영화 리스트
+//영화 리스트
 fetch(`${url}/popular?language=ko-KR&page=1`, options)
-	.then(response => response.json())
-	.then(response => {
-		console.log(response.results)
-		movieList = response.results;
-		response.results.map((res) => {
-			let postImg = `${imgUrl}${res.poster_path}`
-			let movieTitle = res.title;
-			let voteAverage = res.vote_average;
-			let overView = res.overview;
+  .then((response) => response.json())
+  .then((response) => {
+    movieList = response.results;
+    response.results.map((res) => {
+      let postImg =
+        res.poster_path === null ? emptyImg : ` ${imgUrl}${res.poster_path}`;
+      let movieTitle = res.title;
+      let movieName = res.name;
+      let voteAverage = res.vote_average;
+      let overView = res.overview;
 
-			makeMovieCard(res.id, postImg, movieTitle, voteAverage, overView);
-
-		})
-  	})
-  	.catch(err => console.error(err));
+      makeMovieCard(
+        res.id,
+        postImg,
+        movieTitle,
+        movieName,
+        voteAverage,
+        overView
+      );
+    });
+  })
+  .catch((err) => console.error(err));
 
 // 영화 검색
 const enterKey = (event) => {
-	if (event.keyCode === 13) {
-		searchQuery()
-	}
-}
+  if (event.keyCode === 13) {
+    searchQuery();
+  }
+};
 
 const searchQuery = () => {
-	const searchParams = document.querySelector('#search').value;
-	searchParams ? clearCard() : alert("검색어를 입력해주세요")
-	
-	findMovie(searchParams)
-}
+  const searchParams = document.querySelector("#search").value;
+  document.querySelector("#score").value = ``;
+  searchParams ? clearCard() : alert("검색어를 입력해주세요");
+
+  searchResults(searchParams);
+};
 
 const scoreSearch = () => {
-	const inputScore = document.querySelector('#score').value
-	const scoreCheck = document.querySelector('#scoreCheck > option:checked').value
-	
-	const scoreFilter = movieList;
-	
-	clearCard()
-	closeModal()
+  const inputScore = document.querySelector("#score").value;
+  const scoreCheck = document.querySelector(
+    "#scoreCheck > option:checked"
+  ).value;
 
-	scoreFilter.filter((res) => {
-		let score =  res.vote_average
+  const scoreFilter = movieList;
 
-		let movieId = res.id
-		let postImg = `${imgUrl}${res.poster_path}`
-		let movieTitle = res.title;
-		let overView = res.overview;
+  clearCard();
+  closeModal();
 
-		switch (scoreCheck) {
-			case "up": 
-			if (score >= inputScore) {
-				makeMovieCard(movieId,postImg,movieTitle,score,overView);
-			}
-			break;
-			case "down": 
-			if (score <= inputScore) {
-				makeMovieCard(movieId,postImg,movieTitle,score,overView);
-			}
-			break;
-		}
-	})
+  scoreFilter.filter((res) => {
+    let score = res.vote_average;
+    let movieId = res.id;
+    let postImg =
+      res.poster_path === null ? emptyImg : ` ${imgUrl}${item.poster_path}`;
+    let movieTitle = res.title;
+    let overView = res.overview;
 
-}
-
+    switch (scoreCheck) {
+      case "up":
+        if (score >= inputScore) {
+          makeMovieCard(movieId, postImg, movieTitle, score, overView);
+        }
+        break;
+      case "down":
+        if (score <= inputScore) {
+          makeMovieCard(movieId, postImg, movieTitle, score, overView);
+        }
+        break;
+    }
+  });
+};
 
 const clearCard = () => {
-	const cardList = document.querySelectorAll(`.movieCard `)
-	cardList.forEach(element => {
-		element.remove()
-	});
-}
+  const cardList = document.querySelectorAll(`.movieCard `);
+  cardList.forEach((element) => {
+    element.remove();
+  });
+};
 
-const findMovie = (searchParams) => {
-	fetch(`${searchUrl}?query=${searchParams}&include_adult=false&language=ko-KR&page=1`, options)
-		.then(response => response.json())
-		.then(response => {
+const searchResults = (searchParams) => {
+  fetch(
+    `${searchMultiUrl}?query=${searchParams}&include_adult=false&language=ko-KR&page=1`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      response.results.map((item) => {
+        if (item.media_type === "movie") {
+          let postImg =
+            item.poster_path === null
+              ? emptyImg
+              : ` ${imgUrl}${item.poster_path}`;
+          let movieTitle = item.title;
+          let voteAverage = item.vote_average;
+          let overView = item.overview;
 
-		response.results.map((res) => {
-			let postImg = `${imgUrl}${res.poster_path}`
-			let movieTitle = res.title;
-			let voteAverage = res.vote_average;
-			let overView = res.overview;
+          makeMovieCard(item.id, postImg, movieTitle, voteAverage, overView);
+        } else if (item.media_type === "person") {
+          let knownFor = item.known_for;
+          console.log(knownFor);
 
-			makeMovieCard(res.id, postImg, movieTitle, voteAverage, overView);
+          knownFor.filter((item) => {
+            let postImg =
+              item.poster_path === null
+                ? emptyImg
+                : ` ${imgUrl}${item.poster_path}`;
+            let movieTitle = item.title;
+            let voteAverage = item.vote_average.toFixed(2);
+            let overView = item.overview;
 
-		})
-
-  	})
-  	.catch(err => console.error(err));
-
-}
+            makeMovieCard(item.id, postImg, movieTitle, voteAverage, overView);
+          });
+        }
+      });
+    })
+    .catch((err) => console.error(err));
+};
