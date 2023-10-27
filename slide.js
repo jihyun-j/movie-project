ad7c8f033ea23b0e8cde0bafe4a59549daa60108
Park ju won <sogogizmt12@gmail.com>
2023-10-27 16:00:06 +0900

    슬라이드 기능 추가완료

--- slide.js
+++ slide.js
@@ -1,4 +1,4 @@
-const options = {
+const options2 = {
   method: "GET",
   headers: {
     accept: "application/json",
@@ -8,35 +8,43 @@ const options = {
 };
 
 fetch(
-  "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=Ko-kr&page=1&sort_by=popularity.desc",
-  options
+  "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=Ko-Kr&page=1&sort_by=popularity.desc",
+  options2
 )
   .then((response) => response.json())
   .then((response) => {
-    const movie_list = response.results;
-    movie_list.forEach((i) => {
+    const movie_li = response.results;
+    //이미지, 제목 , 줄거리 데이터 가져오기
+    let mainslide = document.querySelector(".slidelist");
+    movie_li.forEach((i) => {
       let image = i["backdrop_path"];
       let title = i["title"];
       let overview = i["overview"];
-
       let slidelist = ` 
-      <div class="slidebox">
-      <ul class="slidelist">
       <li class="slideitem">
         <div>
-        <img src="https://image.tmdb.org/t/p/w500${image}"alt="">
-          <h1>${title}</h1>
-          <p>${overview}</p>
-        </div>
+        <img class = "slideimg" 
+        src="https://image.tmdb.org/t/p/original${image}" alt="">     
+        <h1 class = "image_title" >${title}</h1>
+        <p class = "image-overview">${overview}</p>   
+       </div>
       </li>
-    </ul>
     </div>
-    
       `;
-      let mainslide = document.querySelector(".slidelist");
-      let temp_html = document.createElement("div");
-      temp_html.innerHTML = slidelist;
-      mainslide.append(temp_html);
+
+      // let creatediv = document.createElement("div");
+      // creatediv.innerHTML = slidelist;
+      // mainslide.append(creatediv);
+      mainslide.innerHTML += slidelist;
     });
+
+    setInterval(function () {
+      let randomIndex = Math.floor(Math.random() * mainslide.children.length);
+      for (i = 0; i < mainslide.children.length; i++) {
+        mainslide.children[i].classList.remove("opacity");
+      }
+      mainslide.children[randomIndex].classList.add("opacity");
+    }, 5000);
   })
+
   .catch((err) => console.error(err));
