312b8b86a320ee8b76392f963d1fc88e08962a92
korband <wnsgh2098@gmail.com>
2023-10-27 19:49:28 +0900

    수정 기능 추가 댓글작성후 더보기 버튼 수정

--- review.js
+++ review.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
-    showComments();
-});
+window.onload = function () {
+    loadComments();
+};
 
 function saveReviewWithMovieId() {
     const reviewKey = 댓글;
@@ -12,12 +12,13 @@ function saveReviewWithMovieId() {
 
     let Comment = JSON.parse(localStorage.getItem(reviewKey) || '[]');
 
-    let add1 = document.getElementById('content').value;
-
-    if (!add1) {
+    let add3 = document.getElementById('content').value;
+    let add2 = document.getElementById('password').value;
+    let add1 = document.getElementById('name').value;
+    if (!add1||!add2||!add3) {
         alert('작성창이 빈칸이에요!');
-    } else if (add1.length > 100) {
-        alert('100글자 이하 입력해주세요!');
+    } else if (add1.length < 1 ||add2.length <3||add3.length <3) {
+        alert('닉네임 2글자이상 패스워드 4글자이상 내용 4글자이상!');
     } else {
         let newComment = {
             작성자: name,
@@ -44,7 +45,7 @@ function showComments() {
         <div style="flex-grow: 1"><strong>${Comment.작성자}</strong></div>
         <div style="flex-grow: 5">(${Comment.작성시간})</div>
         <div class="dropdown">
-            <span class="material-symbols-outlined dropBtn">more_vert</span>
+             <span class="material-symbols-outlined dropBtn">more_vert</span>
             <div class="dropdown-content">
                 <button class="editComment" onclick="editComment(${index})">수정</button>
                 <button class="deleteComment" onclick="deleteComment(${index})">삭제</button>
@@ -72,13 +73,19 @@ function deleteComment(index) {
 
 function editComment(index){
     let reviewKey = 댓글;
+    // let Comment = reviewKey[index];
     let Comment = JSON.parse(localStorage.getItem(reviewKey)) || [];
     let 비번 = prompt('비번입력하세여');
     if (비번 === Comment[index].비밀번호) {
-        
+       let edComment= prompt('작성자입력하세여');
+       let edPassword= prompt('바꿀비밀번호입력하세용');
+       let content= prompt('변경할 내용 입력하세용');
+        Comment[index].작성자= edComment ;
+        Comment[index].비밀번호= edPassword ;
+        Comment[index].내용= content ;
+
         localStorage.setItem(reviewKey, JSON.stringify(Comment));
         alert('수정 완료 !')
-       
         showComments();
     }
     else alert('비밀번호 입력해야 수정할수 있어요 !')
